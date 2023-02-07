642bff158efc3de8f61d53d1c26f307af7e7389e
Jennie Smith <Jsmith9spiral@gmail.com>
2023-02-07 11:44:58 -0600

    hello world

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,7 +3,48 @@ package hello;
 public class HelloWorld {
     public static void main(String[] args) {
 //        program starts here!
-        System.out.println("Hello World");
+//        System.out.println("Hello World");
+//        System.out.print("Hello \n");
+//        System.out.print("World");
 //        like console.log
+//        byte mybyte = 127;
+//        mybyte += 1;
+//        System.out.println(mybyte);
+        int myFavoriteNumber = 9;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Hey y'all";
+        System.out.println(myString);
+
+        double myNumber;
+        myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+        int x = 4;
+        x += 5;
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+
+        System.out.println(x);
+//        System.out.println(y);
+        int max = Integer.MAX_VALUE;
+        System.out.println(max);
+
     }
 }
