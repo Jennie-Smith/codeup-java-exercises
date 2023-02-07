089bb9477b549d97c86092903d6f19ad09d4c16a
Jennie Smith <Jsmith9spiral@gmail.com>
2023-02-07 10:16:58 -0600

    another commit

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+//        program starts here!
+        System.out.println("Hello World");
+//        like console.log
+    }
+}
