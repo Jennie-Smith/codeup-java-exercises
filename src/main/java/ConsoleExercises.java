10d0fc95fbe829bc3c84531469bff92e3a9094f8
Jennie Smith <Jsmith9spiral@gmail.com>
2023-02-08 09:52:11 -0600

    update

--- src/main/java/ConsoleExercises.java
+++ src/main/java/ConsoleExercises.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %4.2f.%n", pi);
-
-        Scanner input = new Scanner(System.in);
+        System.out.printf("The value of pi is approximately %.2f.%n", pi);
+//        System.in
+        Scanner input = new Scanner("4 5");
 
 //        System.out.println("Enter number: ");
 //        int numberInput = input.nextInt();
@@ -16,6 +16,7 @@ public class ConsoleExercises {
 //        String bName = input.nextLine();
 //        String cName = input.nextLine();
 //        System.out.println("Your words are: " + aName + " " + bName + " " + cName + ".");
+//        System.out.println("%s%n%s%n%s%n%n", aName, bName, cName);
 
 //        System.out.println("Enter a sentence: ");
 //        String sentence = input.nextLine();
@@ -24,8 +25,10 @@ public class ConsoleExercises {
         int length = input.nextInt();
         System.out.println("Enter Width of room: ");
         int width = input.nextInt();
-        int area = (length * 2) + (width * 2);
-        System.out.println("The area of the room is: " + area);
+        int area = width * length;
+        int perimeter = (length * 2) + (width * 2);
+        System.out.printf("The area is: %d%n The perimeter is: %d%n"
+                , area, perimeter);
 
 
 
